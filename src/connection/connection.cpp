4efcc98db877e180f1d8388f699b2f4edc9961bd
Neyromancer <dmitry.kormulev@yandex.ru>
2018-10-06 08:12:53 +0300

    ip and port fields added. Getters and setters addded for ip and port fields.

--- src/connection/connection.cpp
+++ src/connection/connection.cpp
@@ -5,6 +5,9 @@
 
 #include "connection/connection.h"
 
+#include <arpa/inet.h>
+#include <cstring>
+
 namespace socket_communication {
 namespace connection {
 
@@ -22,8 +25,51 @@ void Connection::SetState(State state) {
   is_connected = state;
 }
 
-bool Connection::IsConnected() {
+inline bool Connection::IsConnected() {
   return is_connected == State.connected;
+}
+
+void Connection::SetIp(const std::string &ip_addr) {
+  if (inet_pton(socket_.GetDomain(), ip_addr.c_str(), ip_) != 1) {
+    // check and if fail log this situation
+    inet_pton(socket_.GetDomain(), "127.0.0.1", ip_);
+  }
+}
+
+void Connection::SetIp(std::string &&ip_addr) {
+  if (inet_pton(socket_.GetDomain(), ip_addr.c_str(), ip_) != 1) {
+    // check and if fail log this situation
+    inet_pton(socket_.GetDomain(), "127.0.0.1", ip_);
+  }
+}
+
+void Connection::SetIp(uint32_t ip_addr) {
+  // check ip_addr value
+  ip_ = ip_addr;
+}
+
+std::string Connection::GetIp() const noexcept {
+  char ip_buf[INET_ADDRSTRLEN] {};
+  if (!inet_ntop(socket_.GetDomain(), ip_, INET_ADDRSTRLEN))
+    return std::string(ip_buf);
+  return std::string();
+}
+
+uint32_t Connection::GetIp() const noexcept {
+  return ip_;
+}
+
+// i want to know if this method called with
+// invalid parameter so i will have check here
+void Connection::SetPort(int32_t port) {
+  if (port > 0 && port < 65535)
+    port_ = port;
+  else // log here
+    port_ = 6600;
+}
+
+uint16_t Connection::GetPort() const noexcept {
+  return port_;
 }
 
 }  // namespace connection
